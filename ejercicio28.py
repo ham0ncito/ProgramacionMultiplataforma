42b6a25b19d6729f0e6a3403e5f8f8e3c8f22bfc
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 14:01:23 -0600

    Progra

--- ejercicio28.py
+++ ejercicio28.py
@@ -4,15 +4,12 @@ for i in range (9):
     x = int(input("Ingrese un valor"))
     lista.append(x)
 print("Los valores ingresados fueron")
-for i in lista:
-    print(i)
-lista = lista.sort()
+print(lista)
+lista.sort()
 print("Los valores ingresados de menor a mayor son")
-for i in lista:
-    print(i)
-lista = lista.sort(reverse=True)
+print(lista)
+lista.sort(reverse=True)
 print("Los valores ingresados de mayor a menor son")
-for i in lista:
-    print(i)
+print(lista)
 
 
