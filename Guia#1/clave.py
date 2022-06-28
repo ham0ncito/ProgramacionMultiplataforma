061a54fff52dfc0c3a20708cbf2643d01f5e035d
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 17:04:06 -0600

    Progra

--- Guia#1/clave.py
+++ Guia#1/clave.py
@@ -3,8 +3,9 @@ y = 1
 for i in range (3):
     x = str(input(" Ingrese la contrasena: "))
     condicional.append(x)
-for i in len(condicional -1):
-    if str.lower(condicional[0]) == str.lower(condicional[y]):
+valor = len(condicional)-1
+for i in range(valor):
+    if str.lower(condicional[0]) == str.lower(condicional[y]) or str.upper(condicional[0]) == str.upper(condicional[y])):
         print(f"La contraseña {y} coincide")
     else:
         print(f"La contraseña {y} no coincide con la original")
