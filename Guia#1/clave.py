34e11d6655162ca45fa100c5ba82a8f1e25fca9f
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 16:58:52 -0600

    Progra

--- Guia#1/clave.py
+++ Guia#1/clave.py
@@ -1,1 +1,11 @@
-condicional = []
+condicional = ["macarronesConQueso22"]
+y = 1
+for i in range (3):
+    x = str(input(" Ingrese la contrasena: "))
+    condicional.append(x)
+for i in len(condicional -1):
+    if str.lower(condicional[0]) == str.lower(condicional[y]):
+        print(f"La contraseña {y} coincide")
+    else:
+        print(f"La contraseña {y} no coincide con la original")
+    y +=1
