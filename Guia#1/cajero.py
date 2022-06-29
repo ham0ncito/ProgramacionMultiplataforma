ff83a0749e5fc159c47f427a1e42a8c581c86bf6
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 19:56:57 -0600

    Progra

--- Guia#1/cajero.py
+++ Guia#1/cajero.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import  os
 cajero = ["Usuario", 15600, "registro.txt"]
 
 def ingreso(x,y):
@@ -19,7 +20,7 @@ with open(cajero[2], 'a') as arc:
     arc.write(f"\n Se ingreso al banco el dia {hora}")
     while(continuar):
         op = str(input("\n\n\t ******* Cajero ******* \n\t 1.Ingresar dinero a la cuenta \n\t 2. Retirar dinero de la cuenta"
-            "\n\t 3. Consultar saldo \n\t Seleccione su opcion: "))
+            "\n\t 3. Consultar saldo \n\t 4. Salir \n\t Seleccione su opcion: "))
         if op == "1":
             cantidad = float(input(" Ingrese la cantidad a depositar: "))
             arc.write(f"\n Se ingresaron {cantidad} lempiras el dia {hora}")
@@ -30,8 +31,12 @@ with open(cajero[2], 'a') as arc:
         elif op == "3":
             print(f" \n\t {cajero[0]} su saldo actual es {float(cajero[1]):.2f}")
             arc.write(f"\n Se consulto el saldo actual de: {cajero[1]} lempiras el dia {hora}")
+        elif op == "4":
+            arc.write(f"\n Se salió del banco el día {hora}")
+            exit()
         else:
             print("\n\t Opcion Incorrecta")
-        cont= str.lower(input(" Desea realizar otra operacion [s/n]"))
+        cont= str.lower(input(" Desea realizar otra operacion [s/n] "))
         if cont != "s":
             continuar = False
+        os.system("cls")
