65e16e512be58b159c383700825cd656f7e6c068
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 19:50:42 -0600

    Progra

--- Guia#1/cajero.py
+++ Guia#1/cajero.py
@@ -1,34 +1,34 @@
 from datetime import datetime
-cajero = ["Usuario", "registro.txt"]
-saldo = 15600
+cajero = ["Usuario", 15600, "registro.txt"]
+
 def ingreso(x,y):
     x +=y
     return  x
-def salida (x,y):
+def salida(x,y):
     if y <= x:
         x -= y
         return x
     else:
-        return ("No hay saldo suficiente")
+        print("\t #### No hay saldo suficiente ####")
 
-with open(cajero[1], 'a') as arc:
+with open(cajero[2], 'a') as arc:
     continuar = True
     hora = datetime.now()
-    arc.write(f"\n Se ingreso al archivo el dia {hora}")
+    arc.write(f"\n Se ingreso al banco el dia {hora}")
     while(continuar):
-        op = str(input("\n\t ******* Cajero ******* \n\t 1.Ingresar dinero a la cuenta \n\t 2. Retirar dinero de la cuenta"
+        op = str(input("\n\n\t ******* Cajero ******* \n\t 1.Ingresar dinero a la cuenta \n\t 2. Retirar dinero de la cuenta"
             "\n\t 3. Consultar saldo \n\t Seleccione su opcion: "))
         if op == "1":
             cantidad = float(input(" Ingrese la cantidad a depositar: "))
             arc.write(f"\n Se ingresaron {cantidad} lempiras el dia {hora}")
-            saldo = (saldo,cantidad)
+            cajero[1] = ingreso(float(cajero[1]),cantidad)
         elif op == "2":
             cantidad = float(input(" Ingrese la cantidad a retirar: "))
             arc.write(f"\n Se retiraron {cantidad} lempiras el dia {hora}")
-            cajero[1] = ingreso(int(cajero[1]), cantidad)
+            cajero[1] = salida(float(cajero[1]), cantidad)
         elif op == "3":
-            print(f" \n\t {cajero[0]} su saldo actual es {saldo:2.f}")
-            arc.write(f"\n Se consulto el saldo actual de: {saldo:.2f} lempiras el dia {hora}")
+            print(f" \n\t {cajero[0]} su saldo actual es {float(cajero[1]):.2f}")
+            arc.write(f"\n Se consulto el saldo actual de: {cajero[1]} lempiras el dia {hora}")
         else:
             print("\n\t Opcion Incorrecta")
         cont= str.lower(input(" Desea realizar otra operacion [s/n]"))
