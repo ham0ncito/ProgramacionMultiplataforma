0a9d280b6ae5b4b734cded560824681c6a506883
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 19:42:17 -0600

    Progra

--- Guia#1/cajero.py
+++ Guia#1/cajero.py
@@ -1,13 +1,36 @@
 from datetime import datetime
-cajero = ["Usuario", 15600, "registro.txt"]
+cajero = ["Usuario", "registro.txt"]
+saldo = 15600
 def ingreso(x,y):
     x +=y
+    return  x
 def salida (x,y):
-    x -= y
-with open(cajero[2], 'a') as arc:
+    if y <= x:
+        x -= y
+        return x
+    else:
+        return ("No hay saldo suficiente")
+
+with open(cajero[1], 'a') as arc:
+    continuar = True
     hora = datetime.now()
     arc.write(f"\n Se ingreso al archivo el dia {hora}")
-    print("\t ******* Cajero ******* \n\t 1.Ingresar dinero a la cuenta \n\t 2. Retirar dinero de la cuenta"
-        "\n\t 3. Consultar saldo \n\t Seleccione su opcion: ")
-
-
+    while(continuar):
+        op = str(input("\n\t ******* Cajero ******* \n\t 1.Ingresar dinero a la cuenta \n\t 2. Retirar dinero de la cuenta"
+            "\n\t 3. Consultar saldo \n\t Seleccione su opcion: "))
+        if op == "1":
+            cantidad = float(input(" Ingrese la cantidad a depositar: "))
+            arc.write(f"\n Se ingresaron {cantidad} lempiras el dia {hora}")
+            saldo = (saldo,cantidad)
+        elif op == "2":
+            cantidad = float(input(" Ingrese la cantidad a retirar: "))
+            arc.write(f"\n Se retiraron {cantidad} lempiras el dia {hora}")
+            cajero[1] = ingreso(int(cajero[1]), cantidad)
+        elif op == "3":
+            print(f" \n\t {cajero[0]} su saldo actual es {saldo:2.f}")
+            arc.write(f"\n Se consulto el saldo actual de: {saldo:.2f} lempiras el dia {hora}")
+        else:
+            print("\n\t Opcion Incorrecta")
+        cont= str.lower(input(" Desea realizar otra operacion [s/n]"))
+        if cont != "s":
+            continuar = False
