fdd38bb2bb305725c806faaf231f5d3b8f7793b7
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 18:34:14 -0600

    Progra

--- Guia#1/cajero.py
+++ Guia#1/cajero.py
@@ -4,8 +4,10 @@ def ingreso(x,y):
     x +=y
 def salida (x,y):
     x -= y
-with open(cajero[2], "r+") as arc:
+with open(cajero[2], 'a') as arc:
+    hora = datetime.now()
+    arc.write(f"\n Se ingreso al archivo el dia {hora}")
     print("\t ******* Cajero ******* \n\t 1.Ingresar dinero a la cuenta \n\t 2. Retirar dinero de la cuenta"
         "\n\t 3. Consultar saldo \n\t Seleccione su opcion: ")
-    hora = datetime.now()
-    arc.write("Se ingreso al archivo el dia ",hora.date()," con hora ", hora.time() )
+
+
