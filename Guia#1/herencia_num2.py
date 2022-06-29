c9240a81a8ec91101be613a35f28d5885296a90e
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 21:03:13 -0600

    Progra

--- Guia#1/herencia_num2.py
+++ Guia#1/herencia_num2.py
@@ -2,7 +2,7 @@ class Persona:
     nombre = ""
     edad = 0
     def mostrar_informacion(self):
-        print(f" \n\t Hola {self.nombre}, de {self.edad} anios")
+        print(f" \n\t Hola {self.nombre}, de {self.edad} anios. Un gusto que trabajes con nosotros \n\n")
     def informacion (self):
         self.nombre = input(" Ingrese su nombre: ")
         self.edad = input(" Ingrese su edad:  ")
@@ -12,7 +12,9 @@ class Empleado(Persona):
     def calcular_suelod(self):
         if self.sueldo > 3000:
             print(f"\n\t {self.nombre}, tiene que pagar impuestos, con una tasa de 15%")
-            print(f"su impuesto a pagar es de {float(self.sueldo * 0.15):.2f}")
+            print(f"\t su impuesto a pagar es de {float(self.sueldo * 0.15):.2f}")
+        else:
+            print(f"\n\t {self.nombre}, no tiene que pagar impuestos todavia")
     def __salario__(self):
         self.sueldo = float(input("Ingrese su salario: "))
 
