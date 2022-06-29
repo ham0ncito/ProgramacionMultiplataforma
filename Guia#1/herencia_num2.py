9b8bc070ec95d6553317c62867958ec1269f972e
ham0ncito <lil_ham0n@hotmail.com>
2022-06-28 20:57:02 -0600

    Progra

--- Guia#1/herencia_num2.py
+++ Guia#1/herencia_num2.py
@@ -3,9 +3,9 @@ class Persona:
     edad = 0
     def mostrar_informacion(self):
         print(f" \n\t Hola {self.nombre}, de {self.edad} anios")
-    def __init__(self,x,y):
-        self.nombre = x
-        self.edad = y
+    def __init__(self):
+        self.nombre = input("Ingrese su nombre")
+        self.edad = input("Ingrese su edad")
 
 class Empleado(Persona):
     sueldo = 0
@@ -13,16 +13,13 @@ class Empleado(Persona):
         if self.sueldo > 3000:
             print(f"\n\t {self.nombre}, tiene que pagar impuestos, con una tasa de 15%")
             print(f"su impuesto a pagar es de {float(self.sueldo * 0.15):.2f}")
-    def __initFuncional__(self,x):
-        self.sueldo = x
+    def __salario__(self):
+        self.sueldo = float(input("Ingrese su salario: "))
 
 Mario = Empleado(Persona)
 
 print("\n\t Programa Herencia")
-nombre = str(input("\n\t Ingrese su nombre: "))
-edad = int(input("\n\t Ingrese su edad: "))
+Mario.__init__()
 Mario.mostrar_informacion()
-salario = float(input("\n\t Ingrese su salario: "))
-Mario.__init__(Persona, nombre,edad)
-Mario.__initFuncional__(salario)
+Mario.__salario__()
 Mario.calcular_suelod()
