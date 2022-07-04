8b4da278670a8ce2f732db8724c6beb54ec6dd1c
ham0ncito <lil_ham0n@hotmail.com>
2022-07-04 11:56:27 -0600

    confimar

--- EjercicioGuia#1.py
+++ EjercicioGuia#1.py
@@ -1,18 +1,14 @@
 numeros_negativos = []
-negativo = True
-for i in range(4):
-    while negativo:
-        numeros_negativos.append(int(input("Ingrese un numero negativo")))
-        if numeros_negativos[i] < 0:
-            negativo = False
-for i in numeros_negativos:
-    print(i)
+y = 1
+while y <= 5:
+        x = int(input("Ingrese un numero negativo "))
+        if x < 0:
+            numeros_negativos.append(x)
+             
+            y+=1
+            continue
+        else:
+            print(("\n\t El valor anterior no es un numero negativo"))
+else:
+    print(numeros_negativos)
 
-x = 1
-lista = [x]
-
-for i in range(0, 20, 1):
-    list.insert(x, i)
-    x += 1
-for i in lista:
-    print(i)
